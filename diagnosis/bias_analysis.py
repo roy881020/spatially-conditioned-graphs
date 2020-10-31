7cbed608e3a69e921dd123dd5a4f1c41f4b4bef5
fred@macbook <frederic.zhang@anu.edu.au>
2020-10-31 14:06:18 +1100

    Add computation of net bias

--- diagnosis/bias_analysis.py
+++ diagnosis/bias_analysis.py
@@ -18,11 +18,21 @@ def summerise(root_dir, files, cache_path):
             idx = torch.nonzero(j == c).squeeze(1)
             n_p = labels[idx].sum()
             n_n = len(idx) - n_p
-            info[str(c)] = (n_p, n_n)
+            info[c.item()] = (n_p.item(), n_n.item())
         info['index'] = data['index']
         all_results.append(info)
     with open(cache_path, 'wb') as f:
         pickle.dump(all_results, f, pickle.HIGHEST_PROTOCOL)
+
+def compute_net_bias(results):
+    bias = torch.zeros(117, 2)
+    normalised_bias = torch.zeros(117, 2)
+    for r in results:
+        class_idx = torch.tensor(list(r.keys()))
+        stats = torch.tensor(list(r.values()))
+        bias[class_idx] += stats
+        normalised_bias[class_idx] += (stats / stats.sum())
+    return bias, normalised_bias
 
 if __name__ == "__main__":
     train_dir = os.path.join(SRC_DIR, 'train2015')
@@ -31,5 +41,18 @@ if __name__ == "__main__":
     train_files = os.listdir(train_dir)
     test_files = os.listdir(test_dir)
 
-    summerise(train_dir, train_files, './stats_train2015.pkl')
-    summerise(test_dir, test_files, './stats_test2015.pkl')
+    train_cache = './stats_train2015.pkl'
+    test_cache = './stats_test2015.pkl'
+
+    if not os.path.exists(train_cache):
+        summerise(train_dir, train_files, train_cache)
+    if not os.path.exists(test_cache):
+        summerise(test_dir, test_files, test_cache)
+
+    # Load total training bias
+    with open(train_cache, 'rb') as f:
+        bias_train2015 = pickle.load(f)
+    # Run analysis
+    bias, normalised_bias = compute_net_bias(bias_train2015)
+
+    torch.save(dict(bias=bias, nbias=normalised_bias), 'net_bias.pt')
