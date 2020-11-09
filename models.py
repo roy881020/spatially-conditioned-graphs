2d79f173ff0522fb6545af4bd510fe3efd2350b4
fred@macbook <frederic.zhang@anu.edu.au>
2020-11-09 18:35:50 +1100

    Add extra box pair predictor

--- models.py
+++ models.py
@@ -135,11 +135,17 @@ class InteractGraphNet(GenericHOINetwork):
         )
 
         box_pair_predictor = nn.Linear(representation_size, num_classes)
+        box_pair_predictor_x = BoxPairPredictor(
+            input_size=node_encoding_size * 2,
+            representation_size=representation_size,
+            num_classes=num_classes
+        )
 
         interaction_head = InteractionHead(
             box_roi_pool=box_roi_pool,
             box_pair_head=box_pair_head,
             box_pair_predictor=box_pair_predictor,
+            box_pair_predictor_x=box_pair_predictor_x,
             num_classes=num_classes,
             human_idx=human_idx,
             box_nms_thresh=box_nms_thresh,
